a5b82e2ea0c20ade271f074159f9191a85be957d
RoyvdAkker94 <van.den.akker.roy@live.nl>
2019-03-20 12:01:47 +0100

    Add css

--- frontend/src/components/ContentBox/ContentBox.tsx
+++ frontend/src/components/ContentBox/ContentBox.tsx
@@ -1,33 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Col } from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Col } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
-import { BannerImage } from '../BannerImage/BannerImage';
-import { Title } from '../Title/Title';
+import { BannerImage } from "../BannerImage/BannerImage";
+import { Title } from "../Title/Title";
+
+type Categories = Array<{
+  id: string;
+  category: string;
+  createdAt: string;
+  updatedAt: string;
+}>;
 
 type Props = {
-  uid: string,
-  image: string,
-  imageAlt: string,
-  title: string,
-  date: string,
-  url: string,
-  description: string
+  uid: string;
+  image: string;
+  imageAlt: string;
+  title: string;
+  date: string;
+  url: string;
+  description: string;
+  categories: Categories;
 };
 
-export const ContentBox = ({ uid, image, imageAlt, title, date, description }: Props) => (
+export const ContentBox = ({
+  uid,
+  image,
+  imageAlt,
+  title,
+  date,
+  description,
+  categories
+}: Props) => (
   <Col sm={12} md={4}>
     <Link to={`/content/${uid}`} className="wrapper">
-      <BannerImage src={image} alt={imageAlt}/>
+      <BannerImage src={image} alt={imageAlt} />
       <div className="description">
-        <Title text={title}/>
+        <Title text={title} />
         <div className="date">
-          <p><FontAwesomeIcon icon={faClock}/> {new Date(date).toLocaleDateString()}</p>
+          <p>
+            <FontAwesomeIcon icon={faClock} />{" "}
+            {new Date(date).toLocaleDateString()}
+          </p>
         </div>
         <div className="subtitle">
           <p>{description}</p>
+        </div>
+        <div className="detail-categorie-wrapper">
+          {categories &&
+            categories.map((category, index) => (
+              <span className="label label-default" key={index}>
+                {category.category.charAt(0).toUpperCase() +
+                  category.category.slice(1)}
+              </span>
+            ))}
         </div>
       </div>
     </Link>
